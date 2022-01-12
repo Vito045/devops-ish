5328425e36d4ebb882916653c418781cdfef3487
Vitaliy Khomenko <vito045@icloud.com>
2022-01-12 22:04:52 +0200

    infinite changes

--- index.js
+++ index.js
@@ -20,47 +20,47 @@ let redisClient = redis.createClient({
   port: REDIS_PORT,
 });
 
-// const postRouter = require('./routes/postRoutes');
-// const userRouter = require('./routes/userRoutes');
+const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// const connectWithRetry = () => {
-//   mongoose
-//     .connect(
-//       `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
-//     )
-//     .then(() => console.log('Succesfully connected to DB'))
-//     .catch((e) => {
-//       console.log(e);
-//       setTimeout(connectWithRetry, 5000);
-//     });
-// };
-// connectWithRetry();
+const connectWithRetry = () => {
+  mongoose
+    .connect(
+      `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+    )
+    .then(() => console.log('Succesfully connected to DB'))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+connectWithRetry();
 
 app.enable('trust proxy');
 app.use(cors({}));
-// app.use(
-//   session({
-//     store: new RedisStore({ client: redisClient }),
-//     secret: SESSION_SECRET,
-//     cookie: {
-//       secure: false,
-//       resave: false,
-//       saveUninitialized: false,
-//       httpOnly: true,
-//       maxAge: 60000,
-//     },
-//   })
-// );
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 60000,
+    },
+  })
+);
 
 app.use(express.json());
 
 app.get('*', (req, res) => {
   res.send('Hi there');
 });
-// app.use('/api/v1/posts', postRouter);
-// app.use('/api/v1/users', userRouter);
+app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
